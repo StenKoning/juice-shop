24256410cceb73e7552af14440e086c328c81eb9
Sten Koning <Sten.Koning@infosupport.com>
2019-04-26 15:06:13 +0200

    Found a solution for previously metioned problem

--- test/appsensor/ie1.test.js
+++ test/appsensor/ie1.test.js
@@ -13,80 +13,7 @@ const appsensor = require('../../appsensor/api')
 const server = require('../../server')
 const checkHeadersForXssPayload = require('../../appsensor/ie1').middleware.checkHeadersForXssPayload
 const clientCore = require('../../appsensor/clientCore')
-
-/*describe('checkHeadersForXssPayload', () => {
-  it('should detect blacklisted XSS payloads in headers and send event to AppSensor', async (done) => {
-    const appsensorReporter = new appsensor.RestReportingEngineApi()
-    const initialEventCountPromise = await appsensorReporter.resourceRestReportingEngineCountEventsGET()
-    const initialEventCount = initialEventCountPromise.response.body
-
-    await request(server.server)
-      .get('/runtime.js')
-      .set('x-forwarded-for', '127.0.0.1')
-      .set('SOME_MALICIOUS_HEADER', '<IMG SRC=javascript:alert(&quot;XSS&quot;)">')
-      .send()
-      .then(function (res) {
-        expect(res).to.have.status(400)
-      })
-
-    const eventCountAfterMaliciousRequestPromise = await appsensorReporter.resourceRestReportingEngineCountEventsGET()
-    const eventCountAfterMaliciousRequest = eventCountAfterMaliciousRequestPromise.response.body
-
-    expect(eventCountAfterMaliciousRequest).to.equal(initialEventCount + 1)
-    done()
-  })
-
-  it('should go next() if no blacklisted payloads are found', async (done) => {
-    const appsensorReporter = new appsensor.RestReportingEngineApi()
-    const initialEventCountPromise = await appsensorReporter.resourceRestReportingEngineCountEventsGET()
-    const initialEventCount = initialEventCountPromise.response.body
-
-    await request(server.server)
-      .get('/runtime.js')
-      .set('x-forwarded-for', '127.0.0.1')
-      .set('SOME_NON_MALICIOUS_HEADER', 'some non malicious value')
-      .send()
-      .then(function (res) {
-        expect(res).to.have.status(200)
-      })
-
-    const eventCountAfterRequestPromise = await appsensorReporter.resourceRestReportingEngineCountEventsGET()
-    const eventCountAfterRequest = eventCountAfterRequestPromise.response.body
-
-    expect(eventCountAfterRequest).to.equal(initialEventCount)
-    done()
-  })
-})
-
-describe('checkBodyForXssPayload', () => {
-  it('should detect blacklisted XSS payloads in HTTP payload and send event to AppSensor on at least POST, PATCH, PUT', async (done) => {
-    const appsensorReporter = new appsensor.RestReportingEngineApi()
-    const initialEventCountPromise = await appsensorReporter.resourceRestReportingEngineCountEventsGET()
-    const initialEventCount = initialEventCountPromise.response.body
-
-    await request(server.server)
-      .post('/api/BasketItems')
-      .set('x-forwarded-for', '127.0.0.1')
-      .send(
-        {
-          'data_with_malicious_value': '<br>Here it comes: <script>alert(document.cookie);</script></br>'
-        }
-      )
-      .then(function (res) {
-        expect(res).to.have.status(400)
-      })
-
-    const eventCountAfterRequestPromise = await appsensorReporter.resourceRestReportingEngineCountEventsGET()
-    const eventCountAfterRequest = eventCountAfterRequestPromise.response.body
-
-    expect(eventCountAfterRequest).to.equal(initialEventCount + 1)
-    done()
-  })
-
-  xit('should be case-insensitive when looking for XSS values', () => {
-    expect(true).to.be.false()
-  })
-})*/
+const insecurity = require('../../lib/insecurity')
 
 let fakeAddAppSensorEventFn
 
@@ -141,22 +68,20 @@ describe('checkHeadersForXssPayload', () => {
   })
 
   it('should respond with HTTP 400 Bad Request', async (done) => {
-    clientCore.commonXssPayloads.forEach((commonXssPayload) => {
-      const request = {
-        headers: {
-          'x-forwarded-for': '127.0.0.1',
-          'some_header': commonXssPayload,
-          'Content-Type': 'application/json'
-        }
-      }
+    /*clientCore.commonXssPayloads.forEach(async (commonXssPayload) => {
 
-      const res = mockRes()
-      checkHeadersForXssPayload(mockReq(request), res, sinon.spy())
-      setTimeout(function () {
-        expect(res.send).to.be.calledWith(408)
-      }, 5)
-    })
+    })*/
 
+    await request(server.server)
+      .get('/api/BasketItems')
+      .set('x-forwarded-for', '127.0.0.1')
+      .set('Authorization', 'Bearer ' + insecurity.authorize())
+      .set('content-type', 'application/json')
+      .set('some_header', '<IMG SRC="javascript:alert(\'XSS\');">')
+      .send()
+      .then(function (res) {
+        expect(res).to.have.status(400)
+      })
     done()
   })
   it('should respond with HTTP 502 Bad Gateway if AppSensor server is unavailable', () => {
