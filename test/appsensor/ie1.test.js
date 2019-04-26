29b506a7cee835057d1a8283d17cce77b11f9930
Sten Koning <Sten.Koning@infosupport.com>
2019-04-26 16:17:55 +0200

    Added behaviour tests for checkBodyForXssPayload and requestContainsMaliciousHeaders

--- test/appsensor/ie1.test.js
+++ test/appsensor/ie1.test.js
@@ -11,9 +11,12 @@ const mockReq = require('sinon-express-mock').mockReq
 const mockRes = require('sinon-express-mock').mockRes
 const appsensor = require('../../appsensor/api')
 const server = require('../../server')
-const checkHeadersForXssPayload = require('../../appsensor/ie1').middleware.checkHeadersForXssPayload
 const clientCore = require('../../appsensor/clientCore')
 const insecurity = require('../../lib/insecurity')
+const Promise = require('bluebird')
+const checkHeadersForXssPayload = require('../../appsensor/ie1').middleware.checkHeadersForXssPayload
+const checkBodyForXssPayload = require('../../appsensor/ie1').middleware.checkBodyForXssPayload
+const requestContainsMaliciousHeaders = require('../../appsensor/ie1').requestContainsMaliciousHeaders
 
 let fakeAddAppSensorEventFn
 
@@ -38,11 +41,9 @@ describe('checkHeadersForXssPayload', () => {
         'Content-Type': 'application/json'
       }
     }
-    const req = mockReq(request)
-    const res = mockRes()
-    const next = sinon.spy()
 
-    checkHeadersForXssPayload(req, res, next)
+    const next = sinon.spy()
+    checkHeadersForXssPayload(mockReq(request), mockRes(), next)
     expect(next).to.be.called
   })
 
@@ -68,10 +69,6 @@ describe('checkHeadersForXssPayload', () => {
   })
 
   it('should respond with HTTP 400 Bad Request', async (done) => {
-    /*clientCore.commonXssPayloads.forEach(async (commonXssPayload) => {
-
-    })*/
-
     await request(server.server)
       .get('/api/BasketItems')
       .set('x-forwarded-for', '127.0.0.1')
@@ -84,32 +81,136 @@ describe('checkHeadersForXssPayload', () => {
       })
     done()
   })
-  it('should respond with HTTP 502 Bad Gateway if AppSensor server is unavailable', () => {
-    clientCore.commonXssPayloads.forEach((commonXssPayload) => {
-      const request = {
-        headers: {
-          'x-forwarded-for': '127.0.0.1',
-          'some_header': commonXssPayload,
-          'Content-Type': 'application/json'
-        }
-      }
+  it('should respond with HTTP 502 Bad Gateway if AppSensor server is unavailable', async (done) => {
+    fakeAddAppSensorEventFn = sinon.fake.returns(Promise.reject())
+    appsensor
+      .RestRequestHandlerApi
+      .prototype
+      .resourceRestRequestHandlerAddEventPOST = fakeAddAppSensorEventFn
 
-      const res = mockRes()
-
-      checkHeadersForXssPayload(mockReq(request), res, sinon.spy())
-      expect(res.send).to.be.calledWith(500)
-    })
+    await request(server.server)
+      .get('/api/BasketItems')
+      .set('x-forwarded-for', '127.0.0.1')
+      .set('Authorization', 'Bearer ' + insecurity.authorize())
+      .set('content-type', 'application/json')
+      .set('some_header', '<IMG SRC="javascript:alert(\'XSS\');">')
+      .send()
+      .then(function (res) {
+        expect(res).to.have.status(502)
+      })
+    done()
   })
 })
 
-/*describe('checkBodyForXssPayload', () => {
-  it('should call next() when there\'s no blacklisted value found in HTTP payload')
-  it('should post a new IE1 event to AppSensor')
-  it('should respond with HTTP 400 Bad Request')
-  it('should respond with HTTP 502 Bad Gateway if AppSensor server is unavailable')
+describe('checkBodyForXssPayload', () => {
+
+  beforeEach(() => {
+    fakeAddAppSensorEventFn = sinon.fake.returns(Promise.resolve({ a: 1 }))
+    appsensor
+      .RestRequestHandlerApi
+      .prototype
+      .resourceRestRequestHandlerAddEventPOST = fakeAddAppSensorEventFn
+  })
+
+  afterEach(() => {
+    sinon.restore()
+  })
+
+  it('should call next() when there\'s no blacklisted value found in HTTP payload', () => {
+    const request = {
+      body: {
+        'some_header': 'some_value',
+        'name': 'Johnny'
+      }
+    }
+
+    const next = sinon.spy()
+    checkHeadersForXssPayload(mockReq(request), mockRes(), next)
+    expect(next).to.be.called
+  })
+
+  it('should post a new IE1 event to AppSensor', () => {
+    clientCore.commonXssPayloads.forEach((commonXssPayload) => {
+      const request = {
+        headers: {
+          'x-forwarded-for': '127.0.0.1'
+        },
+        body: {
+          'name': commonXssPayload
+        }
+      }
+
+      checkBodyForXssPayload(mockReq(request), mockRes(), sinon.spy())
+      expect(fakeAddAppSensorEventFn).to.be.calledWith(
+        sinon.match({
+          detectionPoint: {
+            label: 'IE1'
+          }
+        })
+      )
+    })
+  })
+
+  it('should respond with HTTP 400 Bad Request', async (done) => {
+    await request(server.server)
+      .post('/api/BasketItems')
+      .set('x-forwarded-for', '127.0.0.1')
+      .set('Authorization', 'Bearer ' + insecurity.authorize())
+      .set('content-type', 'application/json')
+      .send(
+        {
+          'name': '<BODY ONLOAD=alert(\'XSS\')>'
+        }
+      )
+      .then(function (res) {
+        expect(res).to.have.status(400)
+      })
+    done()
+  })
+
+  it('should respond with HTTP 502 Bad Gateway if AppSensor server is unavailable', async (done) => {
+    fakeAddAppSensorEventFn = sinon.fake.returns(Promise.reject())
+    appsensor
+      .RestRequestHandlerApi
+      .prototype
+      .resourceRestRequestHandlerAddEventPOST = fakeAddAppSensorEventFn
+
+    await request(server.server)
+      .post('/api/BasketItems')
+      .set('x-forwarded-for', '127.0.0.1')
+      .set('Authorization', 'Bearer ' + insecurity.authorize())
+      .set('content-type', 'application/json')
+      .send(
+        {
+          'name': '<BODY ONLOAD=alert(\'XSS\')>'
+        }
+      )
+      .then(function (res) {
+        expect(res).to.have.status(502)
+      })
+    done()
+  })
 })
 
 describe('requestContainsMaliciousHeaders', () => {
-  it('should return true if given header array contains a value from blacklist')
-  it('should return false if given header array doesn\'t contain a value from blacklist')
-})*/
+  it('should return true if given header array contains a value from blacklist', () => {
+    const request = {
+      headers: {
+        'x-forwarded-for': '127.0.0.1',
+        'some-malicious-header': '$@DSA' + clientCore.commonXssPayloads[0] + '#%#@#'
+      }
+    }
+
+    expect(requestContainsMaliciousHeaders(mockReq(request))).to.be.true
+  })
+  it('should return false if given header array doesn\'t contain a value from blacklist', () => {
+    const request = {
+      headers: {
+        'x-forwarded-for': '127.0.0.1',
+        'some-malicious-header': '$@DSA#%#@#'
+      }
+    }
+
+    expect(requestContainsMaliciousHeaders(mockReq(request))).to.be.false
+  })
+})
