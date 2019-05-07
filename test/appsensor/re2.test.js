670de279c6dc767ea847b1e01cecc34318253ea6
Sten Koning <Sten.Koning@infosupport.com>
2019-05-07 14:52:36 +0200

    Added some code to the re2 integration test

--- test/appsensor/re2.test.js
+++ test/appsensor/re2.test.js
@@ -5,13 +5,35 @@ const expect = chai.expect
 chai.use(sinonChai)
 chai.use(require('chai-datetime'))
 chai.use(require('chai-http'))
+chai.use(require('chai-as-promised'))
+
 const request = chai.request
 const appsensor = require('../../appsensor/api')
 const server = require('../../server')
 const insecurity = require('../../lib/insecurity')
+const Promise = require('bluebird')
 
-describe('unexpectedHttpMethodIsUsed', () => {
-  it('reports to AppSensor when an unexpected method is used for a route & returns HTTP 405', async (done) => {
+describe('Given we are trying to invoke an unusual HTTP Method', () => {
+  it('should do nothing special if HTTP Method is in whitelist', async (done) => {
+    let promisesToMakeRequest = []
+    const allowedMethods = ['HEAD', 'POST', 'PUT', 'DELETE', 'TRACE', 'OPTIONS', 'CONNECT']
+    allowedMethods.forEach((allowedMethod) => {
+      promisesToMakeRequest.push(new Promise((resolve, reject) => {
+        request[allowedMethod.toLowerCase()]('/rest/admin/application-configuration')
+          .set('x-forwarded-for', '127.0.0.1')
+          .set('content-type', 'application/json')
+          .send()
+          .then(function (res) {
+            expect(res).to.have.status(405)
+            resolve(res)
+          })
+          .catch(reject)
+      }))
+    })
+
+    expect(Promise.all(promisesToMakeRequest)).to.be.fulfilled.notify(done)
+  })
+  it('should post a RE2 Event to AppSensor & block request if method not in whitelist', async (done) => {
     request(server.server)
       .copy('/api/BasketItems')
       .set('x-forwarded-for', '127.0.0.1')
@@ -23,11 +45,4 @@ describe('unexpectedHttpMethodIsUsed', () => {
         done()
       })
   })
-})
-
-describe('attemptToInvokeUnsupportedHttpMethod', () => {
-  it('should return next() if the HTTP verb differs from \'HEAD\', \'GET\', \'POST\', \'PUT\', \'DELETE\', \'TRACE\', \'OPTIONS\', \'CONNECT\'')
-  it('should post a new RE2 event to AppSensor')
-  it('should respond with 405 Method Not Allowed')
-  it('should respond with HTTP 502 Bad Gateway if AppSensor server is unavailable')
 })
